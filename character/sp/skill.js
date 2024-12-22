16b54187b778ee812c28b3a4a019eee01fbddeec
MengXinZXZ <2223529500@qq.com>
2024-12-22 23:00:26 +0800

    OL秦朗技能写法修改

--- character/sp/skill.js
+++ character/sp/skill.js
@@ -473,63 +473,59 @@ const skills = {
 				choices = Array.from({ length: hs.length + 1 })
 					.map((_, i) => i)
 					.filter(i => !storage.includes(i));
-			if (hs.length <= Math.max(...storage)) await player.discard(hs);
-			else {
-				player.addTempSkill(record, "roundStart");
-				const {
-					result: { bool, cards },
-				} = await player
-					.chooseToDiscard("he", [1, Infinity], get.prompt(event.name), `你可以弃置任意张牌，若这些牌同名，则本回合结束时你可以视为使用之${storage.length ? `（本轮已弃置过的牌数：${storage}）` : ``}`)
-					.set("filterOk", () => {
-						return !get.event("storage").includes(ui.selected.cards.length);
-					})
-					.set("storage", storage)
-					.set("choices", choices)
-					.set("forced", forced)
-					.set("ai", card => {
-						const { player, storage, names, choices } = get.event();
-						const list = names.filter(i => i[1] > choices.remove(0)[0]).map(i => i[0]);
-						if (list.length) {
-							let num = choices.remove(0)[0];
-							if (ui.selected.cards.length >= num) return 0;
-							if (!list.includes(get.name(card, player))) return 0;
-							return 15 - get.value(card);
-						} else {
-							let num = choices[0];
-							if (ui.selected.cards.length >= num) return 0;
-							return 6 - get.value(card);
-						}
-					})
-					.set(
-						"names",
-						(function () {
-							const hs = player.getCards("he", card => {
-								if (!lib.filter.cardDiscardable(card, player)) return false;
-								return player.hasValueTarget(card) && ["basic", "trick"].includes(get.type(card));
-							});
-							return hs
-								.map(i => get.name(i, player))
-								.reduce((arr, name) => {
-									const index = arr.find(item => item[0] == name);
-									if (!index) arr.push([name, 1]);
-									else index[1]++;
-									return arr;
-								}, [])
-								.sort((a, b) => b[1] - a[1]);
-						})()
-					);
-				if (bool) {
-					player.markAuto(record, [cards.length]);
-					player.storage[record].sort((a, b) => a - b);
-					const names = cards.map(card => get.name(card, player)).toUniqued();
-					if (names.length != 1 || !["basic", "trick"].includes(get.type(names[0]))) return;
-					player.addTempSkill(effect);
-					player.storage[effect].push(names[0]);
-					player.markSkill(effect);
-				} else {
-					if (!forced) player.markAuto(record, [0]);
-					player.storage[record].sort((a, b) => a - b);
-				}
+			player.addTempSkill(record, "roundStart");
+			const result =
+				hs.length <= Math.max(...storage)
+					? { bool: true, cards: hs }
+					: await player
+							.chooseToDiscard("he", [0, Infinity], get.prompt(event.name), `你可以弃置任意张牌，若这些牌同名，则本回合结束时你可以视为使用之${storage.length ? `（本轮已弃置过的牌数：${storage}）` : ``}`)
+							.set("filterOk", () => {
+								return !get.event("storage").includes(ui.selected.cards.length);
+							})
+							.set("storage", storage)
+							.set("choices", choices)
+							.set("forced", forced)
+							.set("ai", card => {
+								const { player, names, choices } = get.event();
+								const list = names.filter(i => i[1] > choices.remove(0)[0]).map(i => i[0]);
+								if (list.length) {
+									let num = choices.remove(0)[0];
+									if (ui.selected.cards.length >= num) return 0;
+									if (!list.includes(get.name(card, player))) return 0;
+									return 15 - get.value(card);
+								} else {
+									let num = choices[0];
+									if (ui.selected.cards.length >= num) return 0;
+									return 6 - get.value(card);
+								}
+							})
+							.set(
+								"names",
+								(function () {
+									const hs = player.getCards("he", card => {
+										if (!lib.filter.cardDiscardable(card, player)) return false;
+										return player.hasValueTarget(card) && ["basic", "trick"].includes(get.type(card));
+									});
+									return hs
+										.map(i => get.name(i, player))
+										.reduce((arr, name) => {
+											const index = arr.find(item => item[0] == name);
+											if (!index) arr.push([name, 1]);
+											else index[1]++;
+											return arr;
+										}, [])
+										.sort((a, b) => b[1] - a[1]);
+								})()
+							);
+			if (result?.bool) {
+				const cards = result.cards || [];
+				player.markAuto(record, [cards.length]);
+				player.storage[record].sort((a, b) => a - b);
+				const names = cards.map(card => get.name(card, player)).toUniqued();
+				if (names.length !== 1 || !["basic", "trick"].includes(get.type(names[0]))) return;
+				player.addTempSkill(effect);
+				player.storage[effect].push(names[0]);
+				player.markSkill(effect);
 			}
 		},
 		ai: {
