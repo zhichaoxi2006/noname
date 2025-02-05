aff9acf565bb44be89500ac26446d94582ca6986
MengXinZXZ <2223529500@qq.com>
2025-02-05 19:05:18 +0800

    耐改王再次修复
    修复【岸然】封牌效果未判定卡牌类别获取tag的报错bug

--- character/clan/skill.js
+++ character/clan/skill.js
@@ -211,16 +211,10 @@ const skills = {
 				},
 				mod: {
 					cardEnabled(card) {
-						if (get.itemtype(card) == "vcard" && Array.isArray(card.cards)) {
-							if (card.cards.some(c => c.hasGaintag("clananran_tag"))) return false;
-						}
-						if (card.hasGaintag("clananran_tag")) return false;
+						if ([card].concat(card.cards || []).some(c => get.itemtype(c) === "card" && c.hasGaintag("clananran_tag"))) return false;
 					},
 					cardSavable(card) {
-						if (get.itemtype(card) == "vcard" && Array.isArray(card.cards)) {
-							if (card.cards.some(c => c.hasGaintag("clananran_tag"))) return false;
-						}
-						if (card.hasGaintag("clananran_tag")) return false;
+						if ([card].concat(card.cards || []).some(c => get.itemtype(c) === "card" && c.hasGaintag("clananran_tag"))) return false;
 					},
 				},
 			},
@@ -257,7 +251,10 @@ const skills = {
 							],
 							true
 						)
-						.set("discarded", discarded.filter(c => target.hasUseTarget(c)))
+						.set(
+							"discarded",
+							discarded.filter(c => target.hasUseTarget(c))
+						)
 						.set("ai", button => {
 							const { player, discarded: cards } = get.event();
 							return {
@@ -268,9 +265,12 @@ const skills = {
 						.forResult()
 				: { bool: true, links: ["loseHp"] };
 			if (result.links[0] == "sha") {
-				const result2 = await target.chooseCardButton("告变：请选择其中一张【杀】使用", discarded, true).set("filterButton", button => {
-					return get.player().hasUseTarget(button.link);
-				}).forResult();
+				const result2 = await target
+					.chooseCardButton("告变：请选择其中一张【杀】使用", discarded, true)
+					.set("filterButton", button => {
+						return get.player().hasUseTarget(button.link);
+					})
+					.forResult();
 				if (result2?.bool && result2.links?.length) await target.chooseUseTarget(result2.links[0], true, false);
 			} else await target.loseHp();
 		},
